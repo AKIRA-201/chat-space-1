19bb700cf130998fad93fbd6f546dbad889eab5b
taniokatomoya <tani@taninoMacBook-puro.local>
2018-03-26 12:22:21 +0900

    Edit autoupdate_messages.js
    What
    自動更新機能の実装
    why
    他の人が投稿した時に自動で更新するため

--- app/assets/javascripts/auto_update_messages.js
+++ app/assets/javascripts/auto_update_messages.js
@@ -1,39 +1,34 @@
 $(function(){
   function buildHTML(message){
-    var img = ""
-    if(message.image !==null){
-      img = `<img src="${message.image}">`
-    }
+    var img
+    message.image == null ? img="" : img=`<img src="${message.image}">`
     var html =
-      `<div class="message" data-message-id="${message.id}">
-      <p class="maincontent__bottom--username">
-      ${message.user_name}
-      </p>
-      <p class="maincontent__bottom--dateandtime">
-      ${message.data}
-      </p>
-      <p class="maincontent__bottom--message">
-      ${message.content}
-      </p>
-      ${img}
-      </div>`
+            `<div class="message" data-message-id="${message.id}">
+            <p class="maincontent__bottom--username">
+            ${message.user_name}
+            </p>
+            <p class="maincontent__bottom--dateandtime">
+            ${message.data}
+            </p>
+            <p class="maincontent__bottom--message">
+            ${message.content}
+            </p>
+            ${img}
+            </div>`
     return html;
-
   }
 
 var interval = setInterval(function(){
   // メッセージのラストのid メッセージがない場合0にする
   if($('.message')[0]){
-  var message_id = $('.message:last').data('message-id');
-   }else{
-    var message_id = 0
-   }
+    var messageId
+    messageId == null ? messageId = 0 : messageId = $('.message:last').data('message-id')
   if(window.location.href.match(/\/groups\/\d+\/messages/)){
     $.ajax({
       url: window.location.href,
       dataType: 'json',
       data: {
-        message: {id: message_id}
+        message: {id: messageId}
       },
       type: "GET",
     })
@@ -47,7 +42,6 @@ var interval = setInterval(function(){
     .fail(function(data){
       alert('更新に失敗しました');
     });
-}
 }else{
   clearInterval(interval);
 }}, 5000);
