28b9ee856360d0dadccefc072299348898ee9691
taniokatomoya <tani@taninoMacBook-puro.local>
2018-03-31 18:03:41 +0900

    edit
    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.1"
 set :linked_files, %w{ config/secrets.yml }
 set :application, "chat-space"
-set :repo_url, "git@example.com:taniokatomoya/chat-space.git"
+set :repo_url, "https://github.com/taniokatomoya/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
